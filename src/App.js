a507fa67e8d9e06f75b3b87c8f301ed5347e1094
mantasjjj <mjakaitis18@gmail.com>
2020-08-12 20:11:36 +0300

    Added conditional rendering to description

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends React.Component {
             <div className="image">
                 <img className="image__img" src={this.state.url}></img>
                 <h3 className="image_title">{this.state.name}</h3>
-                <p className="image_desc">{this.state.desc}</p>
+                <p className="image_desc">{this.state.desc == null ? "This photo does not have a description" : this.state.desc}</p>
             </div>
             
         </div>
