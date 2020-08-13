2c6d70909c90bdc8f08ff889b9fe70ecb7e0e878
mantasjjj <mjakaitis18@gmail.com>
2020-08-13 15:00:25 +0300

    Added card views and previous image, fixed footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ class App extends React.Component {
           name: "Image",
           desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea sequi eligendi omnis maiores ipsa veritatis officia ut ex explicabo suscipit?",
           url: "https://miro.medium.com/max/4000/1*QqfEHlLdVVZIaytTsupcbw.jpeg",
+          prevUrl: "https://miro.medium.com/max/4000/1*QqfEHlLdVVZIaytTsupcbw.jpeg",
+          prevName: "Image",
+          prevDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea sequi eligendi omnis maiores ipsa veritatis officia ut ex explicabo suscipit?",
           data: []
       }
       this.handleClick = this.handleClick.bind(this);
@@ -20,6 +23,14 @@ class App extends React.Component {
 
     handleClick(event){
       event.preventDefault()
+      const previousImg = this.state.url
+      const previousName = this.state.name
+      const previousDesc = this.state.desc
+      this.setState({
+        prevUrl: previousImg,
+        prevName: previousName,
+        prevDesc: previousDesc
+      })
       const randNum = Math.floor(Math.random() * this.state.data.length)
       console.log(randNum)
       const randImg = this.state.data[randNum].urls.regular
@@ -38,10 +49,30 @@ class App extends React.Component {
             <h1 className="content__text">Click Generate to get a random image.</h1>
             <p className="content__para">The images refresh every 5 minutes, so don't worry if you're seeing the same ones.</p>
             <button type="submit" className="btn btn-primary" onClick={this.handleClick}>Generate</button>
-            <div className="image">
-                <img className="image__img" src={this.state.url}></img>
-                <h3 className="image_title">{this.state.name}</h3>
-                <p className="image_desc">{this.state.desc == null ? "This photo does not have a description" : this.state.desc}</p>
+
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-6">
+                <div className="card">
+                  <h5 className="card-time">Previous Image</h5>
+                  <img className="card-img-top" src={this.state.prevUrl} alt="Card image cap"/>
+                  <div className="card-body">
+                    <h5 className="card-title">{this.state.prevName}</h5>
+                    <p className="card-text">{this.state.prevDesc == null ? "This photo does not have a description" : this.state.prevDesc}</p>
+                  </div>
+                </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="card">
+                    <h5 className="card-time">Current Image</h5>
+                    <img className="card-img-top" src={this.state.url} alt="Card image cap"/>
+                    <div className="card-body">
+                      <h5 className="card-title">{this.state.name}</h5>
+                      <p className="card-text">{this.state.desc == null ? "This photo does not have a description" : this.state.desc}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
             
         </div>
@@ -50,3 +81,12 @@ class App extends React.Component {
 }
 
 export default App;
+
+/*
+<div className="image">
+                <img className="image__img" src={this.state.url}></img>
+                <h3 className="image_title">{this.state.name}</h3>
+                <p className="image_desc">{this.state.desc == null ? "This photo does not have a description" : this.state.desc}</p>
+            </div>
+
+            */
